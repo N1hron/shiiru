e9faecb8dcc033d585655ff299eef14d56aaa84e
Denis <n1hron@yandex.com>
2026-02-18 01:46:53 +0300

    Edit settings frame props type

--- src/components/settings/Frame.tsx
+++ src/components/settings/Frame.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/ui/card";
 
 import styles from "./style.module.scss";
 
-type SettingsFrameProps = Omit<ComponentPropsWithRef<typeof Card>, "as">;
+type SettingsFrameProps = Omit<ComponentPropsWithRef<typeof Card<"section">>, "as">;
 
 export function SettingsFrame({ className, ...props }: SettingsFrameProps) {
   const cn = clsx(styles.settings, className);

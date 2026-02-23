276a275ab85b52fcd9382226d2268eaa4b627cd2
Denis <n1hron@yandex.com>
2026-02-23 15:11:38 +0300

    Remove TKey type alias

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -1,6 +1,5 @@
-import type { ParseKeys, TOptions } from "i18next";
+import type { ParseKeys } from "i18next";
 
 export type Theme = "light" | "dark" | "system";
 export type Language = "en" | "ru";
-export type TranslationKey = ParseKeys<["translation"], TOptions>;
-export type TKey = TranslationKey;
+export type TranslationKey = ParseKeys<["translation"]>;

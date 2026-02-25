b0b3ed5221d7032e5b6e152923b8c84ecfa7d904
Denis <n1hron@yandex.com>
2026-02-25 20:43:25 +0300

    Use supportsFileType inside checkDataTransfer

--- src/components/uploader/utils.ts
+++ src/components/uploader/utils.ts
@@ -1,22 +1,10 @@
-import { config } from "@/config";
-
-const acceptParts = new Set(config.uploader.accept);
+import { supportsFileType } from "@/utils/supportsFileType";
 
 export function checkDataTransfer(dataTransfer: DataTransfer) {
-  if (dataTransfer.items.length < 1) {
-    return false;
-  }
-
   for (const item of dataTransfer.items) {
-    if (item.kind === "file" && item.type) {
-      if (acceptParts.has(item.type)) {
-        return true;
-      }
-      if (acceptParts.has(item.type.split("/")[0] + "/*")) {
-        return true;
-      }
+    if (supportsFileType(item)) {
+      return true;
     }
   }
-
   return false;
 }

5355c763153d8ecce62683fcd751edf1ded6ab86
Denis <n1hron@yandex.com>
2026-02-25 19:53:36 +0300

    Add quotes to getFileSignature test description

--- src/utils/__tests__/getFileSignature.test.ts
+++ src/utils/__tests__/getFileSignature.test.ts
@@ -6,7 +6,7 @@ describe("getFileSignature", () => {
       [{ name: "image.png", type: "image/png", size: 500 }, "image.pngimage/png500"],
       [{ name: "video.webp", type: "video/webp", size: 1241513 }, "video.webpvideo/webp1241513"],
       [{ name: "", type: "", size: 1 }, "1"]
-    ])("%o => %s", (file, signature) => {
+    ])("%o => '%s'", (file, signature) => {
       expect(getFileSignature(file as File)).toBe(signature);
     });
   });

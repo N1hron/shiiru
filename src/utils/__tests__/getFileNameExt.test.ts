a2cf02b3ed934833847b5956086172c75c1b9e6d
Denis <n1hron@yandex.com>
2026-02-25 20:11:14 +0300

    Add option to keep extension dot for getFileNameExt

--- src/utils/__tests__/getFileNameExt.test.ts
+++ src/utils/__tests__/getFileNameExt.test.ts
@@ -2,13 +2,28 @@ import { getFileNameExt } from "../getFileNameExt";
 
 describe("getFileNameExt", () => {
   describe("should return file name extension", () => {
-    it.each([
-      ["", ""],
-      ["image", ""],
-      ["image.", ""],
-      ["image.webp", "webp"]
-    ])("'%s' => '%s'", (name, ext) => {
-      expect(getFileNameExt(name)).toBe(ext);
+    describe("should keep dot if second argument == true", () => {
+      it.each([
+        ["", ""],
+        [".", "."],
+        ["image", ""],
+        ["image.", "."],
+        ["image.webp", ".webp"]
+      ])("'%s' => '%s'", (name, ext) => {
+        expect(getFileNameExt(name, true)).toBe(ext);
+      });
+    });
+
+    describe("should remove dot if second argument == false", () => {
+      it.each([
+        ["", ""],
+        [".", ""],
+        ["image", ""],
+        ["image.", ""],
+        ["image.webp", "webp"]
+      ])("'%s' => '%s'", (name, ext) => {
+        expect(getFileNameExt(name)).toBe(ext);
+      });
     });
   });
 });
--- src/utils/getFileNameExt.ts
+++ src/utils/getFileNameExt.ts
@@ -1,3 +1,12 @@
-export function getFileNameExt(name: string) {
-  return name && (name.match(/\.[^.]*$/)?.[0] || "").slice(1);
+export function getFileNameExt(name: string, keepDot: boolean = false) {
+  if (name) {
+    const ext = name.match(/\.[^.]*$/)?.[0];
+    if (ext) {
+      if (keepDot) {
+        return ext;
+      }
+      return ext.slice(1);
+    }
+  }
+  return "";
 }

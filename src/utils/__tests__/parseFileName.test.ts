9aa8addc4b3d3670bffb845841d3f7ef252b5d00
Denis <n1hron@yandex.com>
2026-03-04 19:11:59 +0300

    Fix parseFileName test

--- src/utils/__tests__/parseFileName.test.ts
+++ src/utils/__tests__/parseFileName.test.ts
@@ -3,11 +3,11 @@ import { parseFileName } from "../parseFileName";
 describe("parseFileName", () => {
   describe("should parse file name string into object", () => {
     it.each([
-      ["", { stem: "", ext: "" }],
-      ["image", { stem: "image", ext: "" }],
-      ["image.", { stem: "image", ext: "" }],
-      ["image.png", { stem: "image", ext: "png" }],
-      ["image.png.jpeg", { stem: "image.png", ext: "jpeg" }]
+      ["", { full: "", stem: "", ext: "" }],
+      ["image", { full: "image", stem: "image", ext: "" }],
+      ["image.", { full: "image.", stem: "image", ext: "" }],
+      ["image.png", { full: "image.png", stem: "image", ext: "png" }],
+      ["image.png.jpeg", { full: "image.png.jpeg", stem: "image.png", ext: "jpeg" }]
     ])("'%s' => %o", (name, expected) => {
       expect(parseFileName(name)).toEqual(expected);
     });

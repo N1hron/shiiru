4b6bc8902c3558cd69cebaff1585797b9569d342
Denis <n1hron@yandex.com>
2026-03-07 16:24:46 +0300

    Rename KeysMatching to KeysMatch

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,8 +1,8 @@
-export type KeysMatching<O extends object, V> = {
+export type KeysMatch<O extends object, V> = {
   [K in keyof O]: O[K] extends V ? K : never;
 }[keyof O];
 
-export type PickValues<O extends object, V> = Pick<O, KeysMatching<O, V>>;
+export type PickValues<O extends object, V> = Pick<O, KeysMatch<O, V>>;
 
 export type Values<O extends object> = O[keyof O][];
 
